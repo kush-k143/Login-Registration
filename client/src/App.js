a3d1c33d7d05d6b0218ce0d0ea66c3a423bcb727
Kushank <sisodiya.kushanksingh@gmail.com>
2020-08-16 16:07:10 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React , { Component } from 'react';
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+import NavBar from './Components/NavBar'
+import Landing from './Components/Landing'
+import Register from './Components/Register'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+
+class App extends Component {
+  render() {  
+    return (
+      <Router>
+        <div className="App">
+          <NavBar/>
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+          </div>
+        </div>
+      </Router>  
+    )
+  }
+}
+
+export default App;
